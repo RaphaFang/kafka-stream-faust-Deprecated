87d3cf9013eaca858925709ed4e5ce87e936e6d1
RaphaFang <fangsihyu0211@gmail.com>
2024-09-12 23:10:00 +0800

    test: back to hopping, catch kafka_per_sec_data directly

--- faust_app/ma_agg.py
+++ faust_app/ma_agg.py
@@ -1,19 +1,21 @@
 import faust
 from datetime import datetime
-import pytz
+
 class StockData(faust.Record):
     symbol: str
     type: str
-    MA_type: str
     start: str
     end: str
     current_time: str
-    sma_value: float
-    sum_of_vwap: float
-    count_of_vwap: int
-    window_data_count: int
+    last_data_time: str
     real_data_count: int
     filled_data_count: int
+    real_or_filled: str
+    vwap_price_per_sec: float
+    size_per_sec: int
+    volume_till_now: float
+    yesterday_price: float
+    price_change_percentage: float
 
 class AggregatedData(faust.Record):
     symbol: str
@@ -34,7 +36,7 @@ app = faust.App('stock-ma-data-aggregator',
                 web_port=6066,
                 concurrency=2
             )
-topic = app.topic('kafka_MA_data', value_type=StockData)
+topic = app.topic('kafka_per_sec_data', value_type=StockData)
 windowed_table = app.Table(
     'windowed_stock_table',
     default=lambda: AggregatedData(
@@ -42,52 +44,51 @@ windowed_table = app.Table(
         current_time='', sma_value=0.0, sum_of_vwap=0.0, 
         count_of_vwap=0, window_data_count=0, 
         real_data_count=0, filled_data_count=0
-    ),    # partitions=
-    ).tumbling(size=35) # , step=30
+    )
+).hopping(size=5, step=1)
 
 aggregated_topic = app.topic('kafka_MA_data_aggregated', value_type=AggregatedData)
-
 @app.agent(topic)
 async def process(stream):
-    async for stock_data in stream.group_by(lambda x: f"{x.symbol}_{x.MA_type}", name="grouped_by_symbol_MA"):
-        # 這步驟是建立鍵質
-        key = (stock_data.symbol, stock_data.type, stock_data.MA_type, stock_data.start, stock_data.end)
-        existing = windowed_table[key].value()  ## 這是之前存在的資料
-        
-        if existing is None: ## 如果是空的。把stock_data拉進去放
+    async for stock_data in stream.group_by(lambda x: f"{x.symbol}", name="grouped_by_symbol"):
+        key = (stock_data.symbol, stock_data.type, stock_data.start, stock_data.end)
+        existing = windowed_table[key].value()
+
+        if existing is None:
             windowed_table[key] = AggregatedData(
                 symbol=stock_data.symbol,
                 type=stock_data.type,
-                MA_type=stock_data.MA_type,
+                MA_type='5s_MA',
                 start=stock_data.start,
                 end=stock_data.end,
                 current_time=stock_data.current_time,
-                sma_value=stock_data.sma_value,
-                sum_of_vwap=stock_data.sum_of_vwap,
-                count_of_vwap=stock_data.count_of_vwap,
-                window_data_count=stock_data.window_data_count,
+                sma_value=0.0,
+                sum_of_vwap=stock_data.vwap_price_per_sec * stock_data.size_per_sec,
+                count_of_vwap=stock_data.size_per_sec,
+                window_data_count=1,
                 real_data_count=stock_data.real_data_count,
                 filled_data_count=stock_data.filled_data_count
             )
-        else: ## 如果有。把stock_data加進去放
-            new_sum_of_vwap = existing.sum_of_vwap + stock_data.sum_of_vwap
-            new_count_of_vwap = existing.count_of_vwap + stock_data.count_of_vwap
+        else:
+            new_sum_of_vwap = existing.sum_of_vwap + stock_data.vwap_price_per_sec * stock_data.size_per_sec
+            new_count_of_vwap = existing.count_of_vwap + stock_data.size_per_sec
             new_sma_value = new_sum_of_vwap / new_count_of_vwap if new_count_of_vwap != 0 else 0
 
             windowed_table[key] = AggregatedData(
                 symbol=stock_data.symbol,
                 type=stock_data.type,
-                MA_type=stock_data.MA_type,
+                MA_type='5s_MA',
                 start=stock_data.start,
                 end=stock_data.end,
                 current_time=stock_data.current_time,
                 sma_value=new_sma_value,
                 sum_of_vwap=new_sum_of_vwap,
                 count_of_vwap=new_count_of_vwap,
-                window_data_count=existing.window_data_count + stock_data.window_data_count,
+                window_data_count=existing.window_data_count + 1,
                 real_data_count=existing.real_data_count + stock_data.real_data_count,
                 filled_data_count=existing.filled_data_count + stock_data.filled_data_count
             )
+
         aggregated_data = windowed_table[key].value()
         if aggregated_data:
             await aggregated_topic.send(value=aggregated_data)
@@ -95,3 +96,103 @@ async def process(stream):
 
 if __name__ == '__main__':
     app.main()
+
+
+# --------------------------------------------------------------------------------------------------------
+# import faust
+# from datetime import datetime
+# import pytz
+# class StockData(faust.Record):
+#     symbol: str
+#     type: str
+#     MA_type: str
+#     start: str
+#     end: str
+#     current_time: str
+#     sma_value: float
+#     sum_of_vwap: float
+#     count_of_vwap: int
+#     window_data_count: int
+#     real_data_count: int
+#     filled_data_count: int
+
+# class AggregatedData(faust.Record):
+#     symbol: str
+#     type: str
+#     MA_type: str
+#     start: str
+#     end: str
+#     current_time: str
+#     sma_value: float
+#     sum_of_vwap: float
+#     count_of_vwap: int
+#     window_data_count: int
+#     real_data_count: int
+#     filled_data_count: int
+
+# app = faust.App('stock-ma-data-aggregator',
+#                 broker='kafka://10.0.1.138:9092',
+#                 web_port=6066,
+#                 concurrency=2
+#             )
+# topic = app.topic('kafka_per_sec_data', value_type=StockData)
+# windowed_table = app.Table(
+#     'windowed_stock_table',
+#     default=lambda: AggregatedData(
+#         symbol='', type='', MA_type='', start='', end='',
+#         current_time='', sma_value=0.0, sum_of_vwap=0.0, 
+#         count_of_vwap=0, window_data_count=0, 
+#         real_data_count=0, filled_data_count=0
+#     ),    # partitions=
+#     ).tumbling(size=35) # , step=30
+
+# aggregated_topic = app.topic('kafka_MA_data_aggregated', value_type=AggregatedData)
+
+# @app.agent(topic)
+# async def process(stream):
+#     async for stock_data in stream.group_by(lambda x: f"{x.symbol}_{x.MA_type}", name="grouped_by_symbol_MA"):
+#         # 這步驟是建立鍵質
+#         key = (stock_data.symbol, stock_data.type, stock_data.MA_type, stock_data.start, stock_data.end)
+#         existing = windowed_table[key].value()  ## 這是之前存在的資料
+        
+#         if existing is None: ## 如果是空的。把stock_data拉進去放
+#             windowed_table[key] = AggregatedData(
+#                 symbol=stock_data.symbol,
+#                 type=stock_data.type,
+#                 MA_type=stock_data.MA_type,
+#                 start=stock_data.start,
+#                 end=stock_data.end,
+#                 current_time=stock_data.current_time,
+#                 sma_value=stock_data.sma_value,
+#                 sum_of_vwap=stock_data.sum_of_vwap,
+#                 count_of_vwap=stock_data.count_of_vwap,
+#                 window_data_count=stock_data.window_data_count,
+#                 real_data_count=stock_data.real_data_count,
+#                 filled_data_count=stock_data.filled_data_count
+#             )
+#         else: ## 如果有。把stock_data加進去放
+#             new_sum_of_vwap = existing.sum_of_vwap + stock_data.sum_of_vwap
+#             new_count_of_vwap = existing.count_of_vwap + stock_data.count_of_vwap
+#             new_sma_value = new_sum_of_vwap / new_count_of_vwap if new_count_of_vwap != 0 else 0
+
+#             windowed_table[key] = AggregatedData(
+#                 symbol=stock_data.symbol,
+#                 type=stock_data.type,
+#                 MA_type=stock_data.MA_type,
+#                 start=stock_data.start,
+#                 end=stock_data.end,
+#                 current_time=stock_data.current_time,
+#                 sma_value=new_sma_value,
+#                 sum_of_vwap=new_sum_of_vwap,
+#                 count_of_vwap=new_count_of_vwap,
+#                 window_data_count=existing.window_data_count + stock_data.window_data_count,
+#                 real_data_count=existing.real_data_count + stock_data.real_data_count,
+#                 filled_data_count=existing.filled_data_count + stock_data.filled_data_count
+#             )
+#         aggregated_data = windowed_table[key].value()
+#         if aggregated_data:
+#             await aggregated_topic.send(value=aggregated_data)
+#             print("data sended")
+
+# if __name__ == '__main__':
+#     app.main()
