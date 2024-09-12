1d22609b674587576eb7b1016b5485688b37302c
RaphaFang <fangsihyu0211@gmail.com>
2024-09-12 10:01:26 +0800

    test: set up for data stream

--- faust_app/ma_agg.py
+++ faust_app/ma_agg.py
@@ -1,6 +1,5 @@
 import faust
 from datetime import datetime
-
 class StockData(faust.Record):
     symbol: str
     type: str
@@ -8,19 +7,14 @@ class StockData(faust.Record):
     start: str
     end: str
     current_time: str
-    first_data_time: str
-    last_data_time: str
+    # first_data_time: str
+    # last_data_time: str
     sma_value: float
     sum_of_vwap: float
     count_of_vwap: int
     window_data_count: int
     real_data_count: int
     filled_data_count: int
-
-app = faust.App('stock-ma-data-aggregator', broker='kafka://10.0.1.138:9092')
-
-topic = app.topic('kafka_MA_data', value_type=StockData)
-
 class AggregatedData(faust.Record):
     symbol: str
     type: str
@@ -28,14 +22,22 @@ class AggregatedData(faust.Record):
     start: str
     end: str
     current_time: str
-    first_data_time: str
-    last_data_time: str
+    # first_data_time: str
+    # last_data_time: str
     sma_value: float
     sum_of_vwap: float
     count_of_vwap: int
     window_data_count: int
     real_data_count: int
     filled_data_count: int
+
+app = faust.App('stock-ma-data-aggregator',
+                broker='kafka://10.0.1.138:9092',
+                web_port=6066,
+                concurrency=2
+            )
+
+topic = app.topic('kafka_MA_data', value_type=StockData)
 
 aggregated_topic = app.topic('kafka_MA_data_aggregated', value_type=AggregatedData)
 
@@ -57,8 +59,8 @@ async def process(stream):
                 start=stock_data.start,
                 end=stock_data.end,
                 current_time=stock_data.current_time,
-                first_data_time=stock_data.first_data_time,
-                last_data_time=stock_data.last_data_time,
+                # first_data_time=stock_data.first_data_time,
+                # last_data_time=stock_data.last_data_time,
                 sma_value=stock_data.sma_value,
                 sum_of_vwap=stock_data.sum_of_vwap,
                 count_of_vwap=stock_data.count_of_vwap,
@@ -68,21 +70,21 @@ async def process(stream):
             )
         else:
             existing = table[key]
+
+            new_sum_of_vwap = existing.sum_of_vwap + stock_data.sum_of_vwap
+            new_count_of_vwap = existing.count_of_vwap + stock_data.count_of_vwap
+            new_sma_value = new_sum_of_vwap / new_count_of_vwap if new_count_of_vwap != 0 else 0
+
             table[key] = AggregatedData(
                 symbol=stock_data.symbol,
                 type=stock_data.type,
                 MA_type=stock_data.MA_type,
                 start=stock_data.start,
                 end=stock_data.end,
-                current_time=max(datetime.fromisoformat(stock_data.current_time.replace('+08:00', '')),
-                                 datetime.fromisoformat(existing.current_time.replace('+08:00', ''))).isoformat() + '+08:00',
-                first_data_time=min(datetime.fromisoformat(stock_data.first_data_time.replace('+08:00', '')),
-                                    datetime.fromisoformat(existing.first_data_time.replace('+08:00', ''))).isoformat() + '+08:00',
-                last_data_time=max(datetime.fromisoformat(stock_data.last_data_time.replace('+08:00', '')),
-                                   datetime.fromisoformat(existing.last_data_time.replace('+08:00', ''))).isoformat() + '+08:00',
-                sma_value=existing.sma_value + stock_data.sma_value,
-                sum_of_vwap=existing.sum_of_vwap + stock_data.sum_of_vwap,
-                count_of_vwap=existing.count_of_vwap + stock_data.count_of_vwap,
+                current_time=stock_data.current_time,
+                sma_value=new_sma_value,
+                sum_of_vwap=new_sum_of_vwap,
+                count_of_vwap=new_count_of_vwap,
                 window_data_count=existing.window_data_count + stock_data.window_data_count,
                 real_data_count=existing.real_data_count + stock_data.real_data_count,
                 filled_data_count=existing.filled_data_count + stock_data.filled_data_count
