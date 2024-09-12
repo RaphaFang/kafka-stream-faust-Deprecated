cd6e55aa8bcb98a08f4be589783187d8f09b9456
RaphaFang <fangsihyu0211@gmail.com>
2024-09-12 16:47:35 +0800

    fix: windowed_table[key].value()

--- faust_app/ma_agg.py
+++ faust_app/ma_agg.py
@@ -47,13 +47,14 @@ windowed_table = app.Table(
         real_data_count=0, filled_data_count=0
     ),    # partitions=
     ).hopping(size=60, step=30) 
-
 @app.agent(topic)
 async def process(stream):
-    # async for stock_data in stream.group_by(lambda x: f"{x.symbol}_{x.MA_type}"):
-    async for stock_data in stream.group_by(lambda x: f"{x.symbol}_{x.MA_type}",name="grouped_by_symbol_MA"):
+    async for stock_data in stream.group_by(lambda x: f"{x.symbol}_{x.MA_type}", name="grouped_by_symbol_MA"):
         key = (stock_data.symbol, stock_data.type, stock_data.MA_type, stock_data.start, stock_data.end)
-        if key not in windowed_table:
+        
+        existing = windowed_table[key].value()
+        
+        if existing is None:
             windowed_table[key] = AggregatedData(
                 symbol=stock_data.symbol,
                 type=stock_data.type,
@@ -69,8 +70,6 @@ async def process(stream):
                 filled_data_count=stock_data.filled_data_count
             )
         else:
-            existing = windowed_table[key]
-
             new_sum_of_vwap = existing.sum_of_vwap + stock_data.sum_of_vwap
             new_count_of_vwap = existing.count_of_vwap + stock_data.count_of_vwap
             new_sma_value = new_sum_of_vwap / new_count_of_vwap if new_count_of_vwap != 0 else 0
@@ -89,16 +88,6 @@ async def process(stream):
                 real_data_count=existing.real_data_count + stock_data.real_data_count,
                 filled_data_count=existing.filled_data_count + stock_data.filled_data_count
             )
-        
-        # if windowed_table[key].current_window_expires < datetime.utcnow().timestamp():
-        #     await aggregated_topic.send(value=windowed_table[key]) ## 這邊的會是WindowSet物件
-            
-        # if datetime.fromisoformat(windowed_table[key].current_time) < datetime.utcnow():
-        #     await aggregated_topic.send(value=windowed_table[key])
-        # taipei_tz = pytz.timezone('Asia/Taipei')
-        # current_time = datetime.fromisoformat(current_window.current_time).astimezone(taipei_tz)
-        # if current_window and current_time < datetime.now(taipei_tz):
-        #     await aggregated_topic.send(value=current_window)
 
 
 
